61a269f7e8ecca4fdb19edbecb2b21e1fd3e0773
Charleu Limeira <sdlimeira0@gmail.com>
2024-04-04 21:50:42 -0300

    Criando a classe Pessoa

---
+++ Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Apresentacao
+{
+  
+public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Altura { get; set; }
+
+        public Pessoa(string nome, int idade, double altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Altura = altura;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Apresentacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Pessoa pessoa = new Pessoa("Charleu Limeira", 45, 1.90);
+            Console.WriteLine($"Nome da pessoa: {pessoa.Nome}");
+            Console.WriteLine($"A idade é: {pessoa.Idade} anos");
+            Console.WriteLine($"A altura é: {pessoa.Altura} metros");
+        }
+    }
+}
